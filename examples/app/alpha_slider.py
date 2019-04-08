d2747be0d6b456d0fac16eeedd29674d5883406b
chris <chris@nplan.io>
2019-04-08 16:20:35 +0000

    slider for number of data points

--- examples/app/alpha_slider.py
+++ examples/app/alpha_slider.py
@@ -5,12 +5,6 @@ from bokeh.layouts import layout
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 
-n_data_points = 1000
-sigma = 0.5
-X_left = np.random.normal(loc=(-1, 0), scale=sigma, size=(n_data_points,2))
-X_right = np.random.normal(loc=(1, 0), scale=sigma, size=(n_data_points, 2))
-X = np.concatenate([X_left, X_right])
-
 def update():
     source.data={
         'x': X[:, 0],
@@ -18,6 +12,20 @@ def update():
         'alpha': [alpha_slider.value]*X.shape[0],
         'size': [size_slider.value]*X.shape[0]
     }
+
+def on_datapoints_slider_change(new_value):
+    global X
+    X = generate_data(new_value, sigma)
+    update()
+
+def generate_data(n_data_points, sigma):
+    X_left = np.random.normal(loc=(-1, 0), scale=sigma, size=(n_data_points,2))
+    X_right = np.random.normal(loc=(1, 0), scale=sigma, size=(n_data_points, 2))
+    return np.concatenate([X_left, X_right])
+
+n_data_points = 1000
+sigma = 0.5
+X = generate_data(n_data_points, sigma)
 
 source = ColumnDataSource({
     'x': np.array([]),
@@ -36,11 +44,13 @@ plot.circle(
 )
 alpha_slider = Slider(title="Alpha", start=0, end=1, step=0.01, value=0.2)
 size_slider = Slider(title="Size", start=0, end=50, step=1, value=10)
+datapoints_slider = Slider(title="# Data Points", start=1000, end=50000, step=5000, value=1000)
 alpha_slider.on_change('value', lambda attr, old, new: update())
 size_slider.on_change('value', lambda attr, old, new: update())
+datapoints_slider.on_change('value', lambda attr, old, new: on_datapoints_slider_change(new))
 update()
 layout = layout([
-    [[alpha_slider, size_slider], plot]
+    [[alpha_slider, size_slider, datapoints_slider], plot]
 ])
 document = curdoc()
 document.add_root(layout)
